80e95545bdade7309c9705fdbc6c24977b32deef
JBoundless <39219944+JBoundless@users.noreply.github.com>
2021-05-22 20:20:59 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+import Review from './Review.js';
+
+export default function App() {
+  return (
+    <main>
+    <section className="container">
+      <div className="title">
+      <h2>Our reviews</h2>
+      <div className="underline"></div>
+      </div>
+      <Review />
+    </section>
+    </main>
+  );
+}
---
+++ src/reviews/data.js
@@ -0,0 +1,41 @@
+
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text:
+      "This is an amazing service that really improves my coding skills! I'm impressed!",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'The product is remarkably easy to use and helps my planning immensely!',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    text:
+      'It is truly the next big thing!! So excited for everyone else to try it!',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      'This is an absolute game-changer! If you have not tried it yet, you are missing out on some phenomenal features! My staff and I use have incorporated it into our workflow and it has done wonders for us!',
+  },
+];
+
+export default reviews;
